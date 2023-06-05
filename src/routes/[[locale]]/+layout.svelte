d5c7f360c1e33945c6fd529d25e2bddf914cbd0f
Phillip Malboeuf <phil@phils.computer>
2023-06-05 19:34:52 -0400

    google analytics

--- src/routes/[[locale]]/+layout.svelte
+++ src/routes/[[locale]]/+layout.svelte
@@ -3,7 +3,17 @@
 
   import Header from '$lib/components/Header.svelte'
   import Footer from '$lib/components/Footer.svelte'
+
   import { page } from '$app/stores'
+  import { browser } from '$app/environment'
+
+  $: {
+		if (browser && window.gtag) {
+			window.gtag('config', 'UA-156842255-1', {
+				page_path: $page.url.pathname,
+			})
+		}
+	}
 </script>
 
 {#if !$page.data.film}<Header />{/if}
