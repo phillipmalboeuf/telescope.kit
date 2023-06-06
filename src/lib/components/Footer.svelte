f5b7c16d2523f881eaa8138c466cf0e7124d9779
Phillip Malboeuf <phil@phils.computer>
2023-06-06 16:30:26 -0400

    footer white background

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,38 +9,30 @@
   ]
 </script>
 
-<h1>
-  <Hover texts={['TELESCOPE']} />
-</h1>
+<section>
+  <h1>
+    <Hover texts={['TELESCOPE']} />
+  </h1>
 
-<footer>
-  <a href={`${$page.data.locale === 'fr' ? '/' : `/${$page.data.locale}`}`}><small>© 2023 Télescope</small></a>
+  <footer>
+    <a href={`${$page.data.locale === 'fr' ? '/' : `/${$page.data.locale}`}`}><small>© 2023 Télescope</small></a>
 
-  <nav>
-    {#each links as link}
-    <a href={`${$page.data.locale === 'fr' ? '' : `/${$page.data.locale}`}${link.path}`} rel="external"><small>{link.label}</small></a>
-    {/each}
-    {#if $page.data.locale === 'fr'}
-    <a href="/en"><small>English</small></a>
-    {:else}
-    <a href="/"><small>Français</small></a>
-    {/if}
-  </nav>
-</footer>
+    <nav>
+      {#each links as link}
+      <a href={`${$page.data.locale === 'fr' ? '' : `/${$page.data.locale}`}${link.path}`} rel="external"><small>{link.label}</small></a>
+      {/each}
+      {#if $page.data.locale === 'fr'}
+      <a href="/en"><small>English</small></a>
+      {:else}
+      <a href="/"><small>Français</small></a>
+      {/if}
+    </nav>
+  </footer>
+</section>
 
 <style lang="scss">
   h1 {
-		margin-top: calc(var(--gutter) * 1);
 		text-align: center;
-
-    :global(.dark) & {
-      color: white;
-    }
-
-    @supports (mix-blend-mode: exclusion) {
-      color: white;
-      mix-blend-mode: exclusion;
-    }
 	}
 
 	@media (max-width: 900px) {
@@ -48,21 +40,16 @@
       font-size: 6.33vw;
     }
   }
+
+  section {
+    position: relative;
+    z-index: 22;
+    padding: calc(var(--gutter) / 2);
+    margin-top: calc(var(--gutter) * 1);
+    background-color: white;
+  }
   
   footer {
-    padding: calc(var(--gutter) / 2);
-    // background-color: white;
-
-    :global(.dark) & {
-      color: white;
-      // background-color: transparent;
-    }
-
-    @supports (mix-blend-mode: exclusion) {
-      color: white;
-      mix-blend-mode: exclusion;
-    }
-
     display: flex;
 
     > a {
