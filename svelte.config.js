47ad824d87ea62f834e9553252d5a9c4b37999f3
Phillip Malboeuf <phil@phils.computer>
2024-06-24 17:50:32 -0400

    isr

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// runtime: 'edge'
+			isr: {
+				expiration: false
+			}
 		})
 	}
 };
